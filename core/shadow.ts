7b7e84707a245b7d720f990f4783a255c6ace6c0
Vigilans <vigilans@foxmail.com>
2018-11-26 00:08:02 +0800

    Add object shadow mechanism

---
+++ core/shadow.ts
@@ -0,0 +1,44 @@
+import * as MV from "./MV.js";
+import { Canvas } from "./canvas.js";
+import { WebGLRenderingObject , WebGLUniformMap } from "./webgl-extension.js";
+
+
+declare module "./canvas.js" {
+    interface Canvas {
+        bindShadow(
+            srcObj: WebGLRenderingObject,
+            light: MV.Vector3D,
+            source: { vertSrc: string, fragSrc: string }
+        );
+    }
+}
+
+Object.assign(Canvas.prototype, {
+    bindShadow(this: Canvas, 
+        srcObj: WebGLRenderingObject,        
+        light:MV.Vector3D,
+        source: { vertSrc: string, fragSrc: string }
+    ) {
+        let shadowObj = new WebGLRenderingObject(this.gl);
+        let vShader = this.gl.initShader(source.vertSrc, this.gl.VERTEX_SHADER);
+        let fShader = this.gl.initShader(source.fragSrc, this.gl.FRAGMENT_SHADER);
+        let program = this.gl.initProgram(vShader, fShader);
+
+        shadowObj.bufferInfo = srcObj.bufferInfo;
+
+        shadowObj.programInfo = this.gl.createProgramInfo(program, this.gl.TRIANGLES);
+        shadowObj.uniforms = {
+            u_Color: [0, 0, 0, 1]
+        }
+        this.objectsToDraw.push(shadowObj);
+        this.updatePipeline.push(c => {
+            const T1 = MV.translate(light[0], light[1], light[2]);
+            const M = MV.mat4();
+            M[3][3] = 0;
+            M[3][1] = -1/light[1];
+            const T2 = MV.translate(-light[0], -light[1], -light[2]);
+
+            shadowObj.setModelView(MV.mult(T1, M, T2, srcObj.mvMatrix));
+        });
+    }
+})
