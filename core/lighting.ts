17c69c3ea849bf82589679bc87815cdf4ad031d3
Vigilans <vigilans@foxmail.com>
2018-12-22 12:55:28 +0800

    Refactor lighting

--- core/lighting.ts
+++ core/lighting.ts
@@ -3,75 +3,7 @@ import { Canvas } from "./canvas.js";
 import { WebGLUniformMap, ShaderSource } from "./webgl-extension.js";
 import { WebGLRenderingObject, normRgb } from "./webgl-object.js";
 import { createSphereVertices } from "./3d-geometry.js";
-
-export const LightingShaderSource = {
-    vertSrc: `
-        uniform mat4 u_MVMatrix;
-        uniform mat4 u_WorldMatrix;
-        uniform vec3 u_Lighting;
-        
-        attribute vec4 a_Position;
-        attribute vec3 a_Normal;
-        
-        varying vec3 v_N, v_L, v_E;
-        
-        void main() {
-            
-            vec3 worldPos = (u_MVMatrix * a_Position).xyz;
-            if (u_Lighting.z == 0.0) {
-                v_L = normalize(u_Lighting.xyz);
-            } else {
-                v_L = normalize(u_Lighting).xyz - worldPos;
-            }
-            v_E = -normalize(worldPos);
-            v_N = normalize(mat3(u_MVMatrix) * a_Normal).xyz;
-        
-            gl_Position = u_WorldMatrix * u_MVMatrix * a_Position;
-        }                
-    `,
-    fragSrc: `
-        precision mediump float;
-
-        uniform vec4 u_Ambient;
-        uniform vec4 u_Diffuse;
-        uniform vec4 u_Specular;
-        uniform float u_MShininess;
-        
-        varying vec3 v_N, v_L, v_E;
-        
-        void main() {
-        
-            vec3  H  = normalize(v_L + v_E);
-            float Kd = max(dot(v_N, v_L), 0.0);
-            float Ks = dot(v_L, v_N) > 0.0 ? pow(max(dot(v_N, H), 0.0), u_MShininess) : 0.0;
-        
-            gl_FragColor.rgb = (u_Ambient + u_Diffuse * Kd + u_Specular * Ks).rgb;
-            gl_FragColor.a = 1.0;
-        }
-    `
-};
-
-export const ShadowShaderSource = {
-    vertSrc: `
-        uniform mat4 u_MVMatrix;
-        uniform mat4 u_WorldMatrix;
-
-        attribute vec4 a_Position;
-
-        void main() {
-            gl_Position = u_WorldMatrix * u_MVMatrix * a_Position;
-        }
-    `,
-    fragSrc:`
-        precision mediump float;
-
-        uniform vec4 u_Color;
-        
-        void main() {
-            gl_FragColor = u_Color;
-        }
-    `
-};
+import "./texture.js"
 
 export class WebGLLightingObject extends WebGLRenderingObject {
     constructor(
@@ -80,7 +12,7 @@ export class WebGLLightingObject extends WebGLRenderingObject {
         public ambient: MV.Vector4D, // 环境光
         public diffuse: MV.Vector4D, // 漫反射
         public specular: MV.Vector4D, // 镜面反射
-        public distFactor: MV.Vector3D, // 距离项因子
+        public distFactors: MV.Vector3D = [1, 0, 0], // 距离项因子
         drawSphere = true
     ) {
         super(gl);
@@ -97,9 +29,9 @@ export class WebGLLightingObject extends WebGLRenderingObject {
         }
     }
 
-    distTerm(obj: WebGLRenderingObject) {
+    attenuation(obj: WebGLRenderingObject) {
         const dist = MV.distance(this.center, obj.center);
-        return MV.sum(MV.mult([1, dist, dist*dist], this.distFactor));
+        return 1.0 / MV.sum(MV.mult([1, dist, dist*dist], this.distFactors));
     }
 }
 
@@ -110,13 +42,13 @@ declare module "./canvas.js" {
             ambient: MV.Vector4D,
             diffuse: MV.Vector4D,
             specular: MV.Vector4D,
-            distFactor: MV.Vector3D,
+            distFactors?: MV.Vector3D,
             drawSphere?: boolean
-        );
+        ) : WebGLLightingObject;
         bindShadowObject(
             srcObj: WebGLRenderingObject,
             light: WebGLLightingObject
-        );
+        ) : WebGLRenderingObject;
     }
 }
 
@@ -126,38 +58,44 @@ Object.assign(Canvas.prototype, {
         ambient: MV.Vector4D,
         diffuse: MV.Vector4D,
         specular: MV.Vector4D,
-        distFactor: MV.Vector3D,
+        distFactors: MV.Vector3D = [1, 0, 0],
         drawSphere = true
     ) {
-        let light = new WebGLLightingObject(this.gl, initialPos, ambient, diffuse, specular, distFactor, drawSphere);
+        let light = new WebGLLightingObject(this.gl,
+            initialPos, ambient, diffuse, specular, distFactors, drawSphere
+        );
         this.objectsToDraw.push(light);
         this.updatePipeline.push(c => {
             for (const obj of this.objectsToDraw) {
-                let u_Ambient, u_Diffuse, u_Specular;
-                const distTerm = light.distTerm(obj);
-                if (obj.uniforms.u_Color) {
-                    // 如果对象有标准光照色，则光照起到增强效果
-                    const u_Color = obj.uniforms.u_Color as MV.Vector4D;
-                    u_Ambient  = MV.mult(light.ambient, u_Color); // 环境光不受距离项影响
-                    u_Diffuse  = MV.scale(1 / distTerm, MV.mult(light.diffuse, u_Color));
-                    u_Specular = MV.scale(1 / distTerm, MV.mult(light.specular, u_Color));
-                    if (!obj.uniforms.u_MShininess) { // 如果没有设置高光度，则默认为1
-                        obj.uniforms.u_MShininess = 1.0;
-                    }
-                } else if (["u_MAmbient", "u_MDiffuse", "u_MSpecular", "u_MShininess"].every(p => p in obj.uniforms)) {
-                    // 如果对象有材质属性，则光照决定其最终颜色
-                    const { u_MAmbient, u_MDiffuse, u_MSpecular } = obj.uniforms as { [k:string] : MV.Vector4D };
-                    u_Ambient  = MV.mult(light.ambient, u_MAmbient); // 环境光不受距离项影响
-                    u_Diffuse  = MV.scale(1 / distTerm, MV.mult(light.diffuse, u_MDiffuse));
-                    u_Specular = MV.scale(1 / distTerm, MV.mult(light.specular, u_MSpecular));
-                }
-                Object.assign(obj.uniforms, {
-                    // vertex shader
-                    u_viewMatrix: MV.flatten(this.camera.worldMatrix),
+                let uniforms = {
                     u_Lighting: light.center,
-                    // fragment shader
-                    u_Ambient, u_Diffuse, u_Specular
-                });
+                    u_Ambient:  light.ambient,
+                    u_Diffuse:  light.diffuse,
+                    u_Specular: light.specular,
+                    u_Shininess:  obj.uniforms.u_Shininess || 1.0, // 高光度默认为1.0
+                    u_Attenuation: light.attenuation(obj),
+                    u_DiffuseColor:  [1.0, 1.0, 1.0, 1.0] // DiffuseColor默认为恒等映射
+                } as WebGLUniformMap;
+                // 如果对象有材质属性，则光照决定其最终颜色
+                for (const prop of ["u_Ambient", "u_Diffuse", "u_Specular"]) {
+                    if (prop in obj.uniforms) {
+                        uniforms[prop] = MV.mult(
+                            <MV.Vector4D>uniforms[prop], 
+                            <MV.Vector4D>obj.uniforms[prop]
+                        );
+                    }
+                }
+                // 如果对象有标准颜色，则光照起增强效果
+                if (obj.uniforms.u_Color) {
+                    uniforms.u_DiffuseColor = obj.uniforms.u_Color
+                }
+                // 如果对象有材质，则光照起增强效果
+                if (obj.uniforms.u_DiffuseMap !== undefined) {
+                    // 强化环境光的效果
+                    uniforms.u_Ambient = MV.scale(4.0, <MV.Vector4D>uniforms.u_Ambient);
+                    uniforms.u_Ambient[3] = 1.0;
+                }
+                Object.assign(obj.uniforms, uniforms);
             }
         });
         return light;
@@ -185,7 +123,75 @@ Object.assign(Canvas.prototype, {
             M[3][3] = 0;
             M[3][1] = -1 / lighting.center[1];
             const T2 = MV.translate(...MV.negate(lighting.center));
-            shadowObj.setModelView(MV.mult(T1, M, T2, srcObj.mvMatrix));
+            shadowObj.setModel(MV.mult(T1, M, T2, srcObj.worldMatrix));
         });
     }
 })
+
+export const LightingShaderSource = {
+    vertSrc: `
+        uniform mat4 u_WorldMatrix;
+        uniform mat4 u_ViewMatrix;
+        uniform mat4 u_ProjectionMatrix;
+        uniform vec3 u_Lighting;
+        
+        attribute vec3 a_Position;
+        attribute vec3 a_Normal;
+        
+        varying vec3 v_N, v_L, v_E;
+        
+        void main() {
+            vec3 modelPos = (u_WorldMatrix * vec4(a_Position, 1.0)).xyz;
+            vec3 lightPos = (vec4(u_Lighting, 1.0)).xyz;
+            v_L = normalize(lightPos - modelPos);
+            v_N = normalize(mat3(u_WorldMatrix) * a_Normal.xyz);
+            v_E = -normalize(modelPos);
+        
+            gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_WorldMatrix * vec4(a_Position, 1.0);
+        }                    
+    `,
+    fragSrc: `
+        precision mediump float;
+
+        uniform vec4 u_Ambient;
+        uniform vec4 u_Diffuse;
+        uniform vec4 u_Specular;
+        uniform float u_Attenuation;
+        uniform float u_Shininess;
+        uniform vec4 u_DiffuseColor;
+        
+        varying vec3 v_N, v_L, v_E;
+        
+        void main() {
+        
+            vec3  H  = normalize(v_L + v_E);
+            float Kd = dot(v_N, v_L);
+            float Ks = pow(max(dot(v_N, H), 0.0), u_Shininess);
+        
+            gl_FragColor = u_DiffuseColor * (u_Ambient);
+        }
+    `
+};
+
+export const ShadowShaderSource = {
+    vertSrc: `
+        uniform mat4 u_WorldMatrix;
+        uniform mat4 u_ViewMatrix;
+        uniform mat4 u_ProjectionMatrix;
+        
+        attribute vec4 a_Position;
+        
+        void main() {
+            gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_WorldMatrix * a_Position;
+        }
+    `,
+    fragSrc:`
+        precision mediump float;
+
+        uniform vec4 u_Color;
+        
+        void main() {
+            gl_FragColor = u_Color;
+        }
+    `
+};
